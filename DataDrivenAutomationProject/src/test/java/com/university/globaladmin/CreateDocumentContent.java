e667e1c889929f26d9de8ff812b19acee3e18748
UE7 <UE7@UE7WIN10PLP.dsone.3ds.com>
2019-11-18 17:35:05 +0530

    18th Nov

--- DataDrivenAutomationProject/src/test/java/com/university/globaladmin/CreateDocumentContent.java
+++ DataDrivenAutomationProject/src/test/java/com/university/globaladmin/CreateDocumentContent.java
@@ -5,11 +5,13 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -19,6 +21,7 @@ import com.ds.utilities.CommonUtils;
 import com.ds.utilities.Constants;
 import com.ds.utilities.DataProviders;
 import com.ds.utilities.ExcelReader;
+import com.relevantcodes.extentreports.LogStatus;
 
 
 
@@ -70,22 +73,68 @@ public class CreateDocumentContent extends TestBase{
 		click("createNewContentBtn_XPATH");
 		Thread.sleep(5000);
 		type("contentName_XPATH", data.get("ContentName"));
-		select("contentModeDropDown_XPATH", data.get("Mode"));
-		select("contentTypeDropDown_XPATH", data.get("Type"));
+		
+		//select("contentModeDropDown_XPATH", data.get("Mode"));
+		click("contentModeDropDown_XPATH");		
+		Thread.sleep(2000);
+		if(data.get("Mode").equalsIgnoreCase("Document")) {
+			click("doumentMode_XPATH");
+			test.log(LogStatus.INFO, "Document Mod Selected");
+		}else if(data.get("Mode").equalsIgnoreCase("elearning")) {
+			click("elearningMode_XPATH");
+			test.log(LogStatus.INFO, "elearning Mod Selected");
+		}else if(data.get("Mode").equalsIgnoreCase("instructorLedMode")) {
+			click("instructorLedMode_XPATH");
+			test.log(LogStatus.INFO, "Instructor Led Mod Selected");
+		}else {
+			Assert.fail("couldn't find the Mode");
+		}
+		
+		//select("contentTypeDropDown_XPATH", data.get("Type"));
+		Thread.sleep(2000);
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(or.getProperty("contentTypeDropDown_XPATH"))));
+		click("contentTypeDropDown_XPATH");
+		Thread.sleep(2000);
+		if(data.get("Type").equalsIgnoreCase("Document")) {	
+			//click("documentContentType_XPATH");
+			System.out.println(Keys.ESCAPE);
+			System.out.println("Document Content type is Preselected for Content Mode- Document");
+			test.log(LogStatus.INFO, "Document Content Type Selected");
+		}else if(data.get("Type").equalsIgnoreCase("SCORM")) {
+			click("scormContentType_XPATH");
+			test.log(LogStatus.INFO, "SCORM content type Selected");
+		}else if(data.get("Type").equalsIgnoreCase("training content")) {
+			click("trainingContentType_XPATH");
+			test.log(LogStatus.INFO, "Training content type selected");
+		}
+		
+		//else {
+		//	Assert.fail("couldn't find the Content Type");
+		//}
+		
+		
 		type("durationHH_XPATH",data.get("DurationHH"));
 		type("durationHH_XPATH",data.get("DurationMM"));
-		select("selectSource_XPATH", data.get("Source"));
-		click("ta3DS");
-		click("taBusinessPartner");
-		click("taCSIPartner");
-		click("taPCPartner");
+		
+		
+		//select("selectSource_XPATH", data.get("Source"));
+		click("selectSource_XPATH");
+		click("selectInternalDSSource_XPATH");
+		
+		Thread.sleep(1000);
+		click("ta3DS_XPATH");
+		click("taBusinessPartner_XPATH");
+		click("taCSIPartner_XPATH");
+		click("taPCPartner_XPATH");
 		type("selectTargetRoles_XPATH",data.get("TargetRoles"));
+		Thread.sleep(1000);
 		click("targetRoleQA");
 		type("geoAvailability_XPATH",data.get("GeoAvailability"));
 		click("geoIndia_XPATH");
 		
 		if(data.get("Axis").equalsIgnoreCase("Brand")) {
 		select("selectAxisDropDown_XPATH",data.get("Axis"));
+		Thread.sleep(2000);
 		click("selectBrandBtn_XPATH");
 		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(or.getProperty("selectBrandENOVIA_XPATH"))));
 		click("selectBrandENOVIA_XPATH");
